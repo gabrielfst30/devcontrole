139bc359437c72cfe5394cfcf6e708195442954c
gabrielfst30 <gabrielfst30@gmail.com>
2024-03-22 23:47:14 -0300

    feature: rota dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,16 @@
 import { Container } from "@/components/container";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth"
+import { redirect } from "next/navigation";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const session = await getServerSession(authOptions) //Pegando os dados da sessão
+
+  //caso não esteja logado ou esteja sem dados do usuário, redireciona.
+  if(!session || !session.user){
+    redirect("/")
+  }
+
   return (
     //Englobando todo o conteúdo na página em um componente para estilos especificados
     <Container>
